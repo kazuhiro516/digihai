1983f70b8077f1e834f2e9d24007e0cff4d20dfa
kurokawakazuhiro <k.kurokawa11@gmail.com>
2020-12-23 01:08:41 +0900

    プロフィールページの実装 #7

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,11 @@ module ApplicationHelper
       "#{page_title} | #{BASE_TITLE}"
     end
   end
+
+  def avatar_url(user)
+    return user.profile_photo unless user.profile_photo.nil?
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "https://techpit-market-prod.s3.amazonaws.com/uploads/part_attachment/file/15782/2da91636-af73-4eed-91cd-320a0399609c.jpg"
+  end
+  #user.profile_photoがなければ、デフォルトのアイコンを表示する
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get '/', to: 'static_pages#home'
+  root 'static_pages#home'
   get 'static_pages/about'
   get 'static_pages/terms'
+  get '/users/:id', to: 'users#show', as: 'user'
 end
