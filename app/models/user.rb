8b7cb34e62ff911edb290e85a4f1317844f45333
kurokawakazuhiro <k.kurokawa11@gmail.com>
2020-12-24 09:17:49 +0900

    プロフィール編集機能を実装 #9

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many  :posts, dependent: :destroy
   validates :name, presence: true, length: { maximum: 60 }
+
+  #ユーザー体験を向上させるためパスワードの入力を省略
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update(params, *options)
+    clean_up_passwords
+    result
+  end
 end
