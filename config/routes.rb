a4ad5c1b05d036fce1f0bb25cdf9cd2cf757444c
kurokawakazuhiro <k.kurokawa11@gmail.com>
2020-12-28 10:47:46 +0900

    削除機能の実装 #11

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   get 'static_pages/terms'
   get '/users/:id', to: 'users#show', as: 'user'
-  resources :posts, only: %i(new create index show) do
+  resources :posts, only: %i(new create index show destroy) do
     resources :photos, only: %i(create)
   end
 end
