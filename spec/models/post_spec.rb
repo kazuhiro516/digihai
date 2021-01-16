3cdb3576222e7236e05f97cd6e60b0b6ed0b7afc
kurokawakazuhiro <k.kurokawa11@gmail.com>
2021-01-16 10:13:21 +0900

    Postモデルのテストを追加 #27

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,42 @@
-require 'rails_helper'
-
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  describe '必須項目が入力されていないと投稿できないことを確認' do
+    it '応募条件、登録されたユーザーである場合、有効であること' do
+      post = Post.create(
+        content: "test",
+        title: "sampletitle",
+        user_id: user.id
+      )
+      expect(post).to be_valid
+    end
+
+    it '応募条件がないと無効であること' do
+      post = Post.new(content: nil)
+      post.valid?
+      expect(post.errors[:content]).to include('を入力してください')
+    end
+
+    it '登録されたユーザーでないと無効であること' do
+      post = Post.new(user_id: nil)
+      post.valid?
+      expect(post.errors[:user_id]).to include('を入力してください')
+    end
+  end
+
+  describe '入力した項目が保存できているか確認する' do
+    it '必須項目が入力されていて、保存できること' do
+      post = Post.create(
+        content: "test",
+        title: "sampletitle",
+        user_id: user.id
+      )
+      expect(post.save).to be_truthy
+    end
+
+    it '必須項目が入力されていないので、保存できないこと' do
+      post = Post.new
+      expect(post.save).to be_falsey
+    end
+  end
 end
