222f26204e3c7c7451e998b1b31e4c2b48e3125b
kurokawakazuhiro <k.kurokawa11@gmail.com>
2021-01-16 09:48:58 +0900

    フラッシュメッセージを表示させるため追加 #27

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,20 @@
 class RegistrationsController < Devise::RegistrationsController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      redirect_to root_path
+      flash[:notice] = '登録に成功しました'
+    else
+      flash[:alert] = '必須項目を確認してください'
+      render :new
+    end
+  end
+
   protected
 
   # ユーザー体験を向上させるためパスワードの入力を省略
@@ -8,5 +24,9 @@ class RegistrationsController < Devise::RegistrationsController
 
   def after_update_path_for(resource)
     user_path(resource)
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
   end
 end
