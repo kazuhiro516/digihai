282f400d40925541728bae01a8077d383c46a9ce
kurokawakazuhiro <k.kurokawa11@gmail.com>
2021-01-17 12:03:13 +0900

    reviewモデルのテストを追加 #27

---
+++ spec/factories/private_business_user.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :private_business_user do
+    name { "testuser" }
+  end
+end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,27 @@
-require 'rails_helper'
-
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:review) { create(:review) }
+
+  describe 'レビューを入力していないと投稿できないことを確認する' do
+    it 'レビューが入力されている場合、有効であること' do
+      expect(review).to be_valid
+    end
+
+    it 'レビューが入力されていなければ、無効であること' do
+      review = Review.new(score: nil)
+      review.valid?
+      expect(review.errors[:score]).to include('を入力してください')
+    end
+  end
+
+  describe '入力したレビューが保存されていることを確認する' do
+    it 'レビューが入力されていて、保存できること' do
+      review
+      expect(review.save).to be_truthy
+    end
+
+    it 'レビューが入力されていないので、保存できないこと' do
+      review = Review.new
+      expect(review.save).to be_falsey
+    end
+  end
 end
