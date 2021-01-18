0c8cad6ab8a5312a44367860ca0f983e875ec68f
kurokawakazuhiro <k.kurokawa11@gmail.com>
2021-01-18 09:38:00 +0900

    不要な半角スペースを削除 #30

--- spec/factories/photo.rb
+++ spec/factories/photo.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :photo do
     association :post
-    image   { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test_image.jpg')) }
+    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test_image.jpg')) }
   end
 end
