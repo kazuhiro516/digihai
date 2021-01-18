df8fb391f7f49268b398a163ee63774fca8285ed
kurokawakazuhiro <k.kurokawa11@gmail.com>
2021-01-18 13:55:14 +0900

    テストユーザーに画像を追加 #30

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,26 @@
 # メインユーザーの設定
+profile_photo =Faker::Avatar.image
 User.create!(name:  "Kurokawa Kazuhiro",
              email: "kurokawa@gmail.com",
+             profile_photo: profile_photo,
              password:              "foobar",
              password_confirmation: "foobar")
 
 # ユーザーをまとめて生成する
-100.times do |n|
+50.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@gmail.com"
   password = "password"
+  profile_photo = Faker::Avatar.image
   User.create!(name:  name,
                email: email,
+               profile_photo: profile_photo,
                password:              password,
                password_confirmation: password)
 end
 
-#private_business_userを作成する
-PrivateBusinessUser.create(name: "鈴木浩一")
-PrivateBusinessUser.create(name: "岩田りさ")
-PrivateBusinessUser.create(name: "古川岬")
-PrivateBusinessUser.create(name: "田辺健作")
+# private_business_userを作成する
+50.times do |n|
+  name = Faker::Name.name
+  PrivateBusinessUser.create(name: name)
+end
