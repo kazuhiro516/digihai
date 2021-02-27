569d042f99bcb85a3a49bc873a97a20fa19c759b
kurokawakazuhiro <k.kurokawa11@gmail.com>
2021-02-28 07:45:14 +0900

    不要な間隔を削除 #44

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -2,20 +2,20 @@ RSpec.feature "Users", type: :feature do
   before do
     User.create!(name: 'foobar', email: 'foo@example.com', password: '123456')
   end
-  
+
   scenario 'ログインする' do
     visit new_user_session_path
     fill_in 'メールアドレス', with: 'foo@example.com'
-    fill_in 'パスワード',    with: '123456'
+    fill_in 'パスワード', with: '123456'
     click_on 'ログインする'
     expect(page).to have_content 'サインインしました。'
   end
 
   scenario '新規登録する' do
     visit new_user_registration_path
-    fill_in '※必須：メールアドレス',   with: 'foobar@example.com'
-    fill_in '※必須：名前',           with: 'foobar'
-    fill_in '※必須：パスワード',      with: '123456'
+    fill_in '※必須：メールアドレス', with: 'foobar@example.com'
+    fill_in '※必須：名前', with: 'foobar'
+    fill_in '※必須：パスワード', with: '123456'
     fill_in '※必須：パスワードの確認', with: '123456'
     click_on '登録する'
     expect(page).to have_content '登録に成功しました'
